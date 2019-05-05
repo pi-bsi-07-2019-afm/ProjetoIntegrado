2fef1c6c94489bbdb15e05e319f0deb44c35e094
FelippeLeao <felippe.w.leao@gmail.com>
2019-05-05 13:47:46 -0300

    Sistema atualizado
    No próximo log, informo do que se trata.

--- app/src/main/java/calcinsulina/FMU/projetointegrado/View/TelaCadastro.java
+++ app/src/main/java/calcinsulina/FMU/projetointegrado/View/TelaCadastro.java
@@ -19,7 +19,7 @@ public class TelaCadastro {
     TelaPrincipal tela_principal;
     TextView txtCad;
     Button btnCadastrar;
-    EditText edNome, edIdade, edPeso, edSensibFator, edDataNasc, edEmail;
+    EditText edNome, edPeso, edSensibFator, edDataNasc, edEmail;
     CheckBox cbEULA;
 
     public TelaCadastro (MainActivity act, TelaPrincipal tela_principal) {
@@ -32,7 +32,6 @@ public class TelaCadastro {
         txtCad = act.findViewById(R.id.txtCadastro);
         btnCadastrar = act.findViewById(R.id.btnCadastrar);
         edNome = act.findViewById(R.id.edNome);
-        edIdade = act.findViewById(R.id.edIdade);
         edPeso = act.findViewById(R.id.edPeso);
         edSensibFator = act.findViewById(R.id.edSensibFator);
         cbEULA = act.findViewById(R.id.cbEULA);
@@ -49,15 +48,58 @@ public class TelaCadastro {
                     dialogo.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
+                            String msgErro = "";
+
+                            if (edNome.getText().toString().length() <= 0 ){
+                                msgErro.concat("\n-Informar o nome corretamente.");
+                            }
+
+                            if (edPeso.getText().toString().length() <= 0){
+                                msgErro.concat("\n-Informar o peso corretamente.");
+                            }
+
+                            String[] testaData = edDataNasc.getText().toString().split("/");
+                            if (edDataNasc.getText().toString().length() <= 0 || testaData.length != 3){
+                                if (testaData.length == 3){
+                                    if (testaData[2].length() != 4){
+                                        msgErro.concat("\n-Informar a data de nascimento separando dia, mês e ano por barras. \n\n Exemplo: 01/04/1993");
+                                    }
+                                }else{
+                                    msgErro.concat("\n-Informar a data de nascimento.");
+                                }
+                            }
+
+                            if (edSensibFator.getText().toString().length() <= 0){
+                                msgErro.concat("\n-Informar o fator de sensibilidade.");
+                            }
+
+                            if (edEmail.getText().toString().length() <= 0 || edEmail.getText().toString().indexOf("@") < 0 ){
+                                msgErro.concat("\n-Informar o e-mail corretamente.");
+                            }
+
+                            if (cbEULA.isChecked()){
+
                             String nome = edNome.getText().toString();
                             double peso = Double.parseDouble(edPeso.getText().toString());
                             String dataNasc = edDataNasc.getText().toString();
                             double fatorSensibilidade = Double.parseDouble(edSensibFator.getText().toString());
                             String email = edEmail.getText().toString();
-
                             act.getaUsuario().add(new Usuario(nome, peso, dataNasc, fatorSensibilidade, email));
                             Toast.makeText(act, "Feito o cadastro.", Toast.LENGTH_SHORT).show();
                             act.tela_principal.CarregarTela();
+
+                            }else{
+                                msgErro.concat("\n-Aceitar os termos de uso.");
+                            }
+
+                            if (msgErro.length() > 0){
+                                msgErro = "Para prosseguir, é necessário: \n" + msgErro;
+                                AlertDialog.Builder dialogoValida = new AlertDialog.Builder(act);
+                                dialogoValida.setTitle("Atenção");
+                                dialogoValida.setMessage(msgErro);
+                                dialogoValida.setNeutralButton("Ok", null);
+                                dialogoValida.show();
+                            }
                         }
                     });
                     dialogo.show();
