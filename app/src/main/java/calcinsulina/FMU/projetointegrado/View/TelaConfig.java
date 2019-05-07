ef521ffa4883c783e6dd8a0c269b566fdf1c9d22
Allan <allan.keiiti@gmail.com>
2019-05-06 23:59:07 -0300

    Mudanças de Layout: - Alterado a aparência da tela de calculo - Alterado o MaxLength dos campos da tela de cadastro - Incluso botões na tela de configuração, que são: Alterar cadastro, Alterar calculo, deletar cadastro, voltar e Sobre nós

--- app/src/main/java/calcinsulina/FMU/projetointegrado/View/TelaConfig.java
+++ app/src/main/java/calcinsulina/FMU/projetointegrado/View/TelaConfig.java
@@ -1,17 +1,15 @@
 package calcinsulina.FMU.projetointegrado.View;
 
+import android.app.AlertDialog;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
 import calcinsulina.FMU.projetointegrado.R;
 
 public class TelaConfig {
 
     MainActivity act;
     TelaPrincipal tela_principal;
-    TextView nome, peso, email, datanasc, fatorSenbil;
-    Button button2;
+    Button btnVoltar, btnAltCad, btnAbout;
 
     public TelaConfig(MainActivity act, TelaPrincipal tela_principal){
         this.act = act;
@@ -20,25 +18,41 @@ public class TelaConfig {
 
     public void CarregarTela() {
         act.setContentView(R.layout.tela_config);
-        nome = act.findViewById(R.id.Nome);
-        peso = act.findViewById(R.id.Peso);
-        email = act.findViewById(R.id.email);
-        datanasc = act.findViewById(R.id.dataNasc);
-        fatorSenbil = act.findViewById(R.id.FatorSensibilidade);
-        button2 = act.findViewById(R.id.button2);
+        btnVoltar = act.findViewById(R.id.btnVoltar);
+        btnAltCad = act.findViewById(R.id.btnAltCad);
+        btnAbout = act.findViewById(R.id.btnAbout);
 
-        nome.setText(act.getaUsuario().get(0).getNome());
-        peso.setText(act.getaUsuario().get(0).getDataRegistro());
-        email.setText(act.getaUsuario().get(0).getEmail());
-        datanasc.setText(act.getaUsuario().get(0).getDataNascimento());
-//        fatorSenbil.setText(act.getaUsuario().get(0).getFatorSensibilidade());
-
-        button2.setOnClickListener(new View.OnClickListener() {
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 act.tela_principal.CarregarTela();
             }
         });
+
+        btnAltCad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                act.tela_cadastro.CarregarTela();
+            }
+        });
+
+        btnAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder Dialogo = new AlertDialog.Builder(act);
+                Dialogo.setTitle("Projeto Desenvolido por: ");
+                Dialogo.setMessage("Felippe Wohlers Leão - 8049443\nAllan Keiiti Nakakita - 7925889\nMichel Andrade - 8253225\nMatheus\nNatalia");
+                Dialogo.setNeutralButton("OK", null);
+                Dialogo.show();
+
+            }
+        });
+
+//        nome.setText(act.getaUsuario().get(0).getNome());
+//        peso.setText(act.getaUsuario().get(0).getDataRegistro());
+//        email.setText(act.getaUsuario().get(0).getEmail());
+//        datanasc.setText(act.getaUsuario().get(0).getDataNascimento());
+//        fatorSenbil.setText(act.getaUsuario().get(0).getFatorSensibilidade());
 
     }
 }
